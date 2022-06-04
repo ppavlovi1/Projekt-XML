aca2f63821a58f15bb6546fa1b2945ea73cd50ec
ppavlovi1 <102903091+ppavlovi1@users.noreply.github.com>
2022-06-04 11:37:44 +0200

    Add files via upload

---
+++ Projekt/login.php
@@ -0,0 +1,54 @@
+<?php
+
+$username="";
+$password="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	$ans=$_POST;
+
+	if (empty($ans["username"]))  {
+        	echo "Korisnicki račun nije unesen.";
+		
+    		}
+	else if (empty($ans["password"]))  {
+        	echo "Lozinka nije unesena.";
+		
+    		}
+	else {
+		$username= $ans["username"];
+		$password= $ans["password"];
+	
+		provjera($username,$password);
+	}
+}
+
+
+function provjera($username, $password) {
+	
+
+	$xml=simplexml_load_file("users.xml");
+	
+	
+	foreach ($xml->user as $usr) {
+  	 	$usrn = $usr->username;
+		$usrp = $usr->password;
+		$usrime=$usr->ime;
+		$usrprezime=$usr->prezime;
+		if($usrn==$username){
+			if($usrp == $password){
+				echo "Prijavljeni ste kao $usrime $usrprezime";
+				return;
+				}
+			else{
+				echo "Netocna lozinka";
+				return;
+				}
+			}
+		}
+		
+	echo "Korisnik ne postoji.";
+	return;
+}
+?>
+
